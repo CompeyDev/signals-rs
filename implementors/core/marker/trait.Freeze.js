dbaf8f6a894029d391329e6abe2b650c50f0acfe
Compey <hi@devcomp.xyz>
2023-03-25 17:38:14 +0530

    chore(ci): generated static docs site

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"signals_rs":[["impl Freeze for <a class=\"struct\" href=\"signals_rs/struct.Signal.html\" title=\"struct signals_rs::Signal\">Signal</a>",1,["signals_rs::Signal"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"signals_rs":["",[],["lib.rs"]]\
+}');
+createSourceSidebar();
